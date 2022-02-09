be6866762adef01a46934050b6acb7dadf027d91
Absolomon <andersfs199@gmail.com>
2022-02-09 15:32:30 +0100

    Added en add unit test, pusher for at  få jenkin til at build automatisk

--- CA/SimpleCalculator.Test.Unit/UnitTest1.cs
+++ CA/SimpleCalculator.Test.Unit/UnitTest1.cs
@@ -34,7 +34,17 @@ namespace SimpleCalculator.Test.Unit
         }
 
         [Test]
-        public void Subtract_TwoPosDecNumbers_ResultIsDifference()
+        public void Add_OnePosOneNegDecNumbers_ResultIsNegativeSum()
+        {
+            //Act
+            var result = uut.Add(-3.43, -21.5);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(-24.93));
+        }
+
+        [Test]
+        public void Subtract_TwoPosDecNumbers_ResultIsNegative()
         {
             //Act
             var result = uut.Subtract(21.5, 3.43);
