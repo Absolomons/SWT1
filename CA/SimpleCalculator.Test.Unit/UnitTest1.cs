e69651092a818459f54a61f8b9b7a0eb0ea84f0e
CasperSambleben <Samblebenc@gmail.com>
2022-02-09 16:00:07 +0100

    Divide test IGEN

--- CA/SimpleCalculator.Test.Unit/UnitTest1.cs
+++ CA/SimpleCalculator.Test.Unit/UnitTest1.cs
@@ -103,7 +103,7 @@ namespace SimpleCalculator.Test.Unit
             Assert.That(result, Is.Negative);
         }
         [Test]
-        public void Divide_Two_PosDecNumbers_ResultIsDivided()
+        public void Divide_TwoPosDecNumbers_ResultIsDivided()
         {
             //Act
             var result = uut.Divide(4, 2);
@@ -111,6 +111,26 @@ namespace SimpleCalculator.Test.Unit
             //Assert
             Assert.That(result, Is.EqualTo(2));
         }
+        [Test]
+        public void Divide_PosWithNegNumbers_ResultIsDivided()
+        {
+            //Act
+            var result = uut.Divide(6, -2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(-3));
+        }
+
+        [Test]
+        public void Divide_NegWithNegNumbers_ResultIsDivided()
+        {
+            //Act
+            var result = uut.Divide(-4, -2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
 
         [Test]
         public void Accumulator_CheckTheAccumulatorAfterAdd_AccumulatorIsResult()
